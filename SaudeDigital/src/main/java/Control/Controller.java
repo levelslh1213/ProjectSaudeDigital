d83a82df3c00759a2840eeffc966e627ba3a7dd2
Paulo Paes <pauloleospaes@gmail.com>
2021-10-31 22:43:20 -0300

    =Adding JDBC driver and class controller to DB connection

--- SaudeDigital/src/main/java/Control/Controller.java
+++ SaudeDigital/src/main/java/Control/Controller.java
@@ -5,10 +5,36 @@
  */
 package Control;
 
+import static java.lang.System.out;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 /**
  *
  * @author E181
  */
 public class Controller {
     
+    protected String servidor ="DESKTOP-L1OEKT3";
+    protected String banco="SAUDE_DIGITAL";
+    protected String usuario="sa";
+    protected String senha="orion";
+    
+    public Connection connectDB() throws ClassNotFoundException{
+        Connection connect = null;
+        
+        String url="jdbc:sqlserver://"+servidor+":databaseName="+banco+":user="+usuario+":password="+senha;
+        
+        try {
+            Class.forName("com.microsoft.jdbc.SQLServerDriver");
+            connect = DriverManager.getConnection(url);
+        } catch (ClassNotFoundException e) {
+            out.print(e);
+        } catch (SQLException se){
+            out.print(se);
+        }
+       return connect; 
+    }
 }
