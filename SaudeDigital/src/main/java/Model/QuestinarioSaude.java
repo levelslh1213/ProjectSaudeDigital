a4766cd417691514f54e516477be29118ec903ac
Gabriel <cloudgabriel@protonmail.com>
2021-11-21 14:34:43 -0300

    Created QuestionarioSaude Model Class

---
+++ SaudeDigital/src/main/java/Model/QuestinarioSaude.java
@@ -0,0 +1,226 @@
+package Model;
+
+/**
+ *
+ * @author paulo
+ */
+public class QuestionarioSaude{
+    int idPaciente;
+    private String queixa;
+    private String hitdoenca;
+    private String hemorragia;
+    private String alergia;
+    private String infeccioso;
+    private String cardiovascular;
+    private String gastrite;
+    private String diabetico;
+    private String desmaiou;
+    private String tratamento_med;
+    private String toma_medc;
+    private String doente_5;
+    private String habitos;
+    private String ansie_depre;
+    private String tuberculose; 
+    private String sarampo;
+    private String sifilis;
+    private String caxumba;
+    private String hepatite;
+    private String varicela;
+    private String aids;
+    private String outra_doenca;
+    private String fumanete;
+    private String frequencia; 
+
+    public String getTuberculose() {
+        return tuberculose;
+    }
+
+    public void setTuberculose(String tuberculose) {
+        this.tuberculose = tuberculose;
+    }
+
+    public String getSarampo() { 
+        return sarampo;
+    }
+
+    public void setSarampo(String sarampo) {
+        this.sarampo = sarampo;
+    }
+
+    public String getSifilis() {
+        return sifilis;
+    }
+
+    public void setSifilis(String sifilis) {
+        this.sifilis = sifilis;
+    }
+
+    public String getCaxumba() {
+        return caxumba;
+    }
+
+    public void setCaxumba(String caxumba) {
+        this.caxumba = caxumba;
+    }
+
+    public String getHepatite() {
+        return hepatite;
+    }
+
+    public void setHepatite(String hepatite) {
+        this.hepatite = hepatite;
+    }
+
+    public String getVaricela() {
+        return varicela;
+    }
+
+    public void setVaricela(String varicela) {
+        this.varicela = varicela;
+    }
+
+    public String getAids() {
+        return aids;
+    }
+
+    public void setAids(String aids) {
+        this.aids = aids;
+    }
+
+    public String getOutra_doenca() {
+        return outra_doenca;
+    }
+
+    public void setOutra_doenca(String outra_doenca) {
+        this.outra_doenca = outra_doenca;
+    }
+
+    public String getFumanete() {
+        return fumanete;
+    }
+
+    public void setFumanete(String fumanete) {
+        this.fumanete = fumanete;
+    }
+
+    public String getFrequencia() {
+        return frequencia;
+    }
+
+    public void setFrequencia(String frequencia) {
+        this.frequencia = frequencia;
+    }
+
+     public String getQueixa() {
+        return queixa;
+    }
+
+    public void setQueixa(String queixa) {
+        this.queixa = queixa;
+    }
+
+    public String getHitdoenca() {
+        return hitdoenca;
+    }
+
+    public void setHitdoenca(String hitdoenca) {
+        this.hitdoenca = hitdoenca;
+    }
+
+    public String getHemorragia() {
+        return hemorragia;
+    }
+
+    public void setHemorragia(String hemorragia) {
+        this.hemorragia = hemorragia;
+    }
+
+    public String getAlergia() {
+        return alergia;
+    }
+
+    public void setAlergia(String alergia) {
+        this.alergia = alergia;
+    }
+
+    public String getInfeccioso() {
+        return infeccioso;
+    }
+
+    public void setInfeccioso(String infeccioso) {
+        this.infeccioso = infeccioso;
+    }
+
+    public String getCardiovascular() {
+        return cardiovascular;
+    }
+
+    public void setCardiovascular(String cardiovascular) {
+        this.cardiovascular = cardiovascular;
+    }
+
+    public String getGastrite() {
+        return gastrite;
+    }
+
+    public void setGastrite(String gastrite) {
+        this.gastrite = gastrite;
+    }
+
+    public String getDiabetico() {
+        return diabetico;
+    }
+
+    public void setDiabetico(String diabetico) {
+        this.diabetico = diabetico;
+    }
+
+    public String getDesmaiou() {
+        return desmaiou;
+    }
+
+    public void setDesmaiou(String desmaiou) {
+        this.desmaiou = desmaiou;
+    }
+
+    public String getTratamento_med() {
+        return tratamento_med;
+    }
+
+    public void setTratamento_med(String tratamento_med) {
+        this.tratamento_med = tratamento_med;
+    }
+
+    public String getToma_medc() {
+        return toma_medc;
+    }
+
+    public void setToma_medc(String toma_medc) {
+        this.toma_medc = toma_medc;
+    }
+
+    public String getDoente_5() {
+        return doente_5;
+    }
+
+    public void setDoente_5(String doente_5) {
+        this.doente_5 = doente_5;
+    }
+
+    public String getHabitos() {
+        return habitos;
+    }
+
+    public void setHabitos(String habitos) {
+        this.habitos = habitos;
+    }
+
+    public String getAnsie_depre() {
+        return ansie_depre;
+    }
+
+    public void setAnsie_depre(String ansie_depre) {
+        this.ansie_depre = ansie_depre;
+    }
+    
+}
